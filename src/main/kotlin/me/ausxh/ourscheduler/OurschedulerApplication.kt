5ed37b76343f4e18b3a097521d1251690ee5a155
Ausxh <ausxh66@gmail.com>
2022-11-20 15:38:38 -0800

    used spring initializer?

---
+++ src/main/kotlin/me/ausxh/ourscheduler/OurschedulerApplication.kt
@@ -0,0 +1,11 @@
+package me.ausxh.ourscheduler
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OurschedulerApplication
+
+fun main(args: Array<String>) {
+	runApplication<OurschedulerApplication>(*args)
+}
---
+++ src/test/kotlin/me/ausxh/ourscheduler/OurschedulerApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.ausxh.ourscheduler
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OurschedulerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
