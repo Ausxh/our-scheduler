c5c6a952135bff0297b53c45156faddb120e6759
Harjot Singh <harjot.singh0712@gmail.com>
2023-07-20 19:51:04 -0700

    Clean up imports

--- src/main/kotlin/me/ausxh/ourscheduler/controller/HtmlController.kt
+++ src/main/kotlin/me/ausxh/ourscheduler/controller/HtmlController.kt
@@ -4,15 +4,12 @@ import me.ausxh.ourscheduler.repository.*
 import me.ausxh.ourscheduler.model.Course
 import me.ausxh.ourscheduler.model.Subject
 
-import org.springframework.http.ResponseEntity
-import org.springframework.beans.factory.annotation.Autowired
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.CookieValue
+
 
 import java.util.UUID
 
--- src/main/kotlin/me/ausxh/ourscheduler/repository/AppUserRepository.kt
+++ src/main/kotlin/me/ausxh/ourscheduler/repository/AppUserRepository.kt
@@ -1,8 +1,6 @@
 package me.ausxh.ourscheduler.repository;
 
 import me.ausxh.ourscheduler.model.AppUser;
-import me.ausxh.ourscheduler.model.Course;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import java.util.UUID
