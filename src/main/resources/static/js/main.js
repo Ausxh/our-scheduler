d6ec62f4e1bcdec9a08956887a330b7509945bc7
Harjot Singh <harjot.singh0712@gmail.com>
2023-07-20 19:52:40 -0700

    Add generate button functionality and display

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -5,3 +5,30 @@ function getCookieValue(name)
         return match[2]
     }
 }
+
+function generateSchedules() 
+{
+    var scheduleList = document.getElementById('scheduleList')
+    const xhttp = new XMLHttpRequest();
+
+    // Define a callback function
+    xhttp.onload = function() {
+        var response = JSON.parse(xhttp.responseText)
+
+        for (var i = 0; i < response.length; i++) {
+        var div = document.createElement("div")
+            for(var j = 0; j < response[i].length; j++) {
+                course = response[i][j]
+                div.innerHTML += course.subject.title + " " + course.start_time + " " + course.end_time + " " + course.days + "<br>"
+            }
+            scheduleList.appendChild(div)
+        }
+    }
+
+    // Send a request
+    xhttp.open("GET", "generate");
+    xhttp.setRequestHeader("Content-Type", "application/json");
+    xhttp.setRequestHeader("X-CSRFToken", getCookieValue("csrftoken"));
+    xhttp.send();
+
+}
